d15a09851cf3e657a6f1218d6e905f9957f40a97
Eurêka <therealeureka@protonmail.com>
2023-01-14 09:40:30 +0100

    Gerard majax

--- public/data/js/map.js
+++ public/data/js/map.js
@@ -20,31 +20,37 @@ const markerpurple = L.icon({
 });
 //console.log(L.Icon.Default);
 
-
-let map = L.map('map').setView([48.6880561, 6.1559293], 13);
-
-
-
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+let parkings = L.layerGroup([]);
+let myPoints = L.layerGroup([]);
+let baselayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-}).addTo(map);
+})
 
+
+
+let map = L.map('map',{
+    layers: [baselayer, parkings, myPoints]
+}).setView([48.6880561, 6.1559293], 13);
 
 fetch('https://geoservices.grand-nancy.org/arcgis/rest/services/public/VOIRIE_Parking/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=nom%2Cadresse%2Cplaces%2Ccapacite&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson'
 ).then(response => response.json()).then(data => {
-    data.features.forEach(feature => {
-        let marker = L.marker([feature.geometry.y, feature.geometry.x]).addTo(map);
-        marker.bindPopup(feature.attributes.NOM + '<br>' + feature.attributes.ADRESSE + '<br>' + feature.attributes.PLACES + '/' + feature.attributes.CAPACITE + ' places');
+    data.features.forEach(point => {
+        let marker = L.marker([point.geometry.y, point.geometry.x]);
         points.push(marker);
+        let txt = point.attributes.NOM + '<br>' + point.attributes.ADRESSE;
+        if(point.attributes.PLACES && point.attributes.CAPACITE){
+            txt += '<br>' + point.attributes.PLACES + '/' + point.attributes.CAPACITE + ' places';
+        }
+        marker.bindPopup(txt);
+        parkings.addLayer(marker);
     });
+
     display_points()
 
 });
 function onMapClick(e) {
     let marker =L.marker([ e.latlng.lat, e.latlng.lng], {icon : markerpurple});
-     marker.addTo(map);
-
+    myPoints.addLayer(marker);
     let label = prompt("Label");
     if(label){
      marker.bindPopup(label);
@@ -56,12 +62,22 @@ function onMapClick(e) {
 
 function display_points(){
     let html = '';
-    console.log(points);
     for(let i = 0; i < points.length; i++){
         let point = points[i];
-        console.log(point)
-        html += '<li>' + point.getPopup().getContent() + '</li>';
+        if(point.getPopup()) {
+            html += '<li>' + point.getPopup().getContent() + '</li>';
+        }
     }
     document.getElementById('points').innerHTML = html;
 }
+
+let overlayMaps = {
+    "Parkings": parkings,
+    "Mes points": myPoints
+};
+
+
+L.control.layers({}, overlayMaps).addTo(map);
+
 map.on('click', onMapClick);
+
